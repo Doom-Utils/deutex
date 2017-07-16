9f8438ae51e2967c23252c0cfc9b70b73e2d89f7
Mike Swanson <mikeonthecomputer@gmail.com>
2017-07-15 20:57:29 -0700

    endian*c: Reapply the correct LGPL declaration
    I accidentally replaced it with the full GPL when I updated the license
    texts in 2017-07-14T08:44:54Z!mikeonthecomputer@gmail.com

--- src/endianio.c
+++ src/endianio.c
@@ -15,15 +15,15 @@
 /*
 This file is Copyright © 1999-2005 André Majorel.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2.1 of the
+License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 */
 
 
